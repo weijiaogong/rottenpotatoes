8532ecf5aa2671f48210337dfb4c5cd8032f6e55
weijiaogong <weijiaogong@gmail.com>
2016-10-13 19:55:38 -0500

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,21 +3,42 @@ class MoviesController < ApplicationController
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
-
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
-  def index
-    if params[:id]
-	column = params[:id].gsub(/_header/,'')
-    	@movies = Movie.order(column + " asc")
+
+  def get_checked_ratings(ratings)
+    if ratings
+       ratings.each do |rating, active|
+         @all_ratings[rating] = true
+       end    
+    end
+    checked_ratings = @all_ratings.select {|k,v| v == true}
+
+    if checked_ratings.empty?
+       @movies = Movie.all
     else
-        @movies = Movie.all
+       @movies = Movie.filter(checked_ratings)
+    end
+
+  end
+
+  def index
+
+    @all_ratings = {'G' => false, 'PG' => false, 'PG-13' => false, 'R' => false}
+
+    get_checked_ratings(params[:ratings])
+
+    if /_header$/ =~ params[:id]
+	column = params[:id].gsub(/_header$/,'')
+    	@movies = @movies.order(column + " asc")
     end
   end
+
 
   def new
     # default: render 'new' template
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
 
+	def self.filter(ratings)
+            where(rating: ratings.keys) 
+	end
+
 end
